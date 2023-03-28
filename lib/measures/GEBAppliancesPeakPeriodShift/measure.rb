9a198209d7cfa2e98c1fcbd386bb7bd838074ebd
Joe Robertson <joseph.robertson@nrel.gov>
2023-03-28 16:13:26 -0700

    Get schedule file shifting minimally working.

--- lib/measures/GEBAppliancesPeakPeriodShift/measure.rb
+++ lib/measures/GEBAppliancesPeakPeriodShift/measure.rb
@@ -70,7 +70,7 @@ class GEBAppliancesPeakPeriodShift < OpenStudio::Measure::ModelMeasure
     end
 
     schedules_peak_period = runner.getStringArgumentValue('schedules_peak_period', user_arguments)
-    schedules_peak_period_delay = runner.getIntegerArgumentValue('schedules_peak_period_delay', user_arguments)
+    delay = runner.getIntegerArgumentValue('schedules_peak_period_delay', user_arguments)
 
     schedule_file_column_names_enabled = {}
     get_schedule_file_column_names(model).each do |schedule_file_column_name|
@@ -82,6 +82,23 @@ class GEBAppliancesPeakPeriodShift < OpenStudio::Measure::ModelMeasure
       return true
     end
 
+    begin_hour, end_hour = Schedules.parse_time_range(schedules_peak_period)
+
+    if begin_hour >= end_hour
+      runner.registerError("Specified peak period (#{begin_hour} - #{end_hour}) must be at least one hour long.")
+      return false
+    end
+
+    if ((end_hour - begin_hour) + delay > 12)
+      runner.registerError("Specified peak period (#{begin_hour} - #{end_hour}), plus the delay (#{delay}), must be no longer than 12 hours.")
+      return false
+    end
+
+    total_days_in_year = 365 # TODO
+    sim_year = 2009 # TODO
+    sim_start_day = DateTime.new(sim_year, 1, 1)
+    steps_in_day = 24 # TODO
+
     # TODO: use schedule_files, schedules_peak_period, schedules_peak_period_delay to shift referenced ScheduleFile objects
     # 1 create ScheduleFile class for loading externalFile into hash
     # 2 do the operations from https://github.com/NREL/OpenStudio-HPXML/pull/1293 on applicable columns
@@ -90,7 +107,7 @@ class GEBAppliancesPeakPeriodShift < OpenStudio::Measure::ModelMeasure
       external_file_path = external_file.filePath.to_s
 
       schedules = Schedules.new(file_path: external_file_path)
-      schedules.do_something(schedule_file_column_names_enabled)
+      schedules.shift_schedules(schedule_file_column_names_enabled, begin_hour, end_hour, delay, total_days_in_year, sim_start_day, steps_in_day)
       schedules.export()
     end
 
@@ -123,14 +140,52 @@ class Schedules
     end
   end
   
-  def do_something(schedule_file_column_names_enabled)
+  def shift_schedules(schedule_file_column_names_enabled, begin_hour, end_hour, delay, total_days_in_year, sim_start_day, steps_in_day)
+    unshifted = {}
     schedule_file_column_names_enabled.each do |schedule_file_column_name, peak_period_shift_enabled|
       next if !@schedules.keys.include?(schedule_file_column_name)
+      next if !peak_period_shift_enabled
 
-      @schedules[schedule_file_column_name][0] *= 1.1 if peak_period_shift_enabled
+      schedule = @schedules[schedule_file_column_name]
+      unshifted[schedule_file_column_name] = 0
+      
+      total_days_in_year.times do |day|
+        today = sim_start_day + day
+        day_of_week = today.wday
+        next if [0, 6].include?(day_of_week)
+
+        shifted = day_peak_shift(schedule, day, begin_hour, end_hour, delay, steps_in_day)
+        unshifted[schedule_file_column_name] += 1 if !shifted
+      end
+    end
+
+    unshifted.each do |schedule_file_column_name, days|
+      next if days == 0
+
+      puts "To prevent stacking, #{days} days were not shifted for the '#{schedule_file_column_name}' schedule."
     end
   end
   
+  def day_peak_shift(schedule, day, begin_hour, end_hour, delay, steps_in_day)
+    steps_in_hour = steps_in_day / 24
+    period = (end_hour - begin_hour) * steps_in_hour # n steps
+
+    # peak period
+    peak_begin_ix = day * steps_in_day + (begin_hour * steps_in_hour)
+    peak_end_ix = peak_begin_ix + period
+
+    # new period
+    new_begin_ix = peak_end_ix + (delay * steps_in_hour)
+    new_end_ix = new_begin_ix + period
+
+    return false if schedule[new_begin_ix...new_end_ix].any? { |x| x > 0 } # prevent stacking
+
+    schedule[new_begin_ix...new_end_ix] = schedule[peak_begin_ix...peak_end_ix]
+    schedule[peak_begin_ix...peak_end_ix] = [0] * period
+
+    return true
+  end
+
   def export()
     CSV.open(@file_path, 'wb') do |csv|
       csv << @schedules.keys
@@ -143,6 +198,18 @@ class Schedules
   
   def schedules
     return @schedules
+  end
+
+  def self.parse_time_range(time_range)
+    begin_end_times = time_range.split('-').map { |v| v.strip }
+    if begin_end_times.size != 2
+      fail "Invalid time format specified for '#{time_range}'."
+    end
+
+    begin_hour = begin_end_times[0].strip.to_i
+    end_hour = begin_end_times[1].strip.to_i
+
+    return begin_hour, end_hour
   end
 end
 
--- lib/measures/GEBAppliancesPeakPeriodShift/tests/GEBAppliancesPeakPeriodShift_Test.rb
+++ lib/measures/GEBAppliancesPeakPeriodShift/tests/GEBAppliancesPeakPeriodShift_Test.rb
@@ -55,7 +55,7 @@ class GEBAppliancesPeakPeriodShiftTest < Minitest::Test
     count = -1
 
     schedules_peak_period = arguments[count += 1].clone
-    assert(schedules_peak_period.setValue('15 - 18'))
+    assert(schedules_peak_period.setValue('17 - 19'))
     argument_map['schedules_peak_period'] = schedules_peak_period
 
     schedules_peak_period_delay = arguments[count += 1].clone
@@ -63,7 +63,7 @@ class GEBAppliancesPeakPeriodShiftTest < Minitest::Test
     argument_map['schedules_peak_period_delay'] = schedules_peak_period_delay
 
     schedules_peak_period_ceiling_fan = arguments[count += 1].clone
-    assert(schedules_peak_period_ceiling_fan.setValue(true))
+    assert(schedules_peak_period_ceiling_fan.setValue(false))
     argument_map['schedules_peak_period_ceiling_fan'] = schedules_peak_period_ceiling_fan
 
     schedules_peak_period_clothes_dryer = arguments[count += 1].clone
@@ -83,43 +83,43 @@ class GEBAppliancesPeakPeriodShiftTest < Minitest::Test
     argument_map['schedules_peak_period_dishwasher'] = schedules_peak_period_dishwasher
 
     schedules_peak_period_hot_water_clothes_washer = arguments[count += 1].clone
-    assert(schedules_peak_period_hot_water_clothes_washer.setValue(true))
+    assert(schedules_peak_period_hot_water_clothes_washer.setValue(false))
     argument_map['schedules_peak_period_hot_water_clothes_washer'] = schedules_peak_period_hot_water_clothes_washer
 
     schedules_peak_period_hot_water_dishwasher = arguments[count += 1].clone
-    assert(schedules_peak_period_hot_water_dishwasher.setValue(true))
+    assert(schedules_peak_period_hot_water_dishwasher.setValue(false))
     argument_map['schedules_peak_period_hot_water_dishwasher'] = schedules_peak_period_hot_water_dishwasher
 
     schedules_peak_period_hot_water_fixtures = arguments[count += 1].clone
-    assert(schedules_peak_period_hot_water_fixtures.setValue(true))
+    assert(schedules_peak_period_hot_water_fixtures.setValue(false))
     argument_map['schedules_peak_period_hot_water_fixtures'] = schedules_peak_period_hot_water_fixtures
 
     schedules_peak_period_lighting_garage = arguments[count += 1].clone
-    assert(schedules_peak_period_lighting_garage.setValue(true))
+    assert(schedules_peak_period_lighting_garage.setValue(false))
     argument_map['schedules_peak_period_lighting_garage'] = schedules_peak_period_lighting_garage
 
     schedules_peak_period_lighting_interior = arguments[count += 1].clone
-    assert(schedules_peak_period_lighting_interior.setValue(true))
+    assert(schedules_peak_period_lighting_interior.setValue(false))
     argument_map['schedules_peak_period_lighting_interior'] = schedules_peak_period_lighting_interior
 
     schedules_peak_period_occupants = arguments[count += 1].clone
-    assert(schedules_peak_period_occupants.setValue(true))
+    assert(schedules_peak_period_occupants.setValue(false))
     argument_map['schedules_peak_period_occupants'] = schedules_peak_period_occupants
 
     schedules_peak_period_outage = arguments[count += 1].clone
-    assert(schedules_peak_period_outage.setValue(true))
+    assert(schedules_peak_period_outage.setValue(false))
     argument_map['schedules_peak_period_outage'] = schedules_peak_period_outage
 
     schedules_peak_period_plug_loads_other = arguments[count += 1].clone
-    assert(schedules_peak_period_plug_loads_other.setValue(true))
+    assert(schedules_peak_period_plug_loads_other.setValue(false))
     argument_map['schedules_peak_period_plug_loads_other'] = schedules_peak_period_plug_loads_other
 
     schedules_peak_period_plug_loads_tv = arguments[count += 1].clone
-    assert(schedules_peak_period_plug_loads_tv.setValue(true))
+    assert(schedules_peak_period_plug_loads_tv.setValue(false))
     argument_map['schedules_peak_period_plug_loads_tv'] = schedules_peak_period_plug_loads_tv
 
     schedules_peak_period_vacancy = arguments[count += 1].clone
-    assert(schedules_peak_period_vacancy.setValue(true))
+    assert(schedules_peak_period_vacancy.setValue(false))
     argument_map['schedules_peak_period_vacancy'] = schedules_peak_period_vacancy
 
     # before
@@ -149,7 +149,7 @@ class GEBAppliancesPeakPeriodShiftTest < Minitest::Test
 
     assert(!schedules_before.empty?)
     assert(!schedules_after.empty?)
-    assert(schedules_before['ceiling_fan'][0] != schedules_after['ceiling_fan'][0])
+    assert(schedules_before['ceiling_fan'][0] == schedules_after['ceiling_fan'][0])
     assert(schedules_before['clothes_dryer'][0] == schedules_after['clothes_dryer'][0])
     assert(schedules_before['clothes_washer'][0] == schedules_after['clothes_washer'][0])
     assert(schedules_before['cooking_range'][0] == schedules_after['cooking_range'][0])
@@ -157,11 +157,11 @@ class GEBAppliancesPeakPeriodShiftTest < Minitest::Test
     assert(schedules_before['hot_water_clothes_washer'][0] == schedules_after['hot_water_clothes_washer'][0])
     assert(schedules_before['hot_water_dishwasher'][0] == schedules_after['hot_water_dishwasher'][0])
     assert(schedules_before['hot_water_fixtures'][0] == schedules_after['hot_water_fixtures'][0])
-    assert(schedules_before['lighting_garage'][0] != schedules_after['lighting_garage'][0])
-    assert(schedules_before['lighting_interior'][0] != schedules_after['lighting_interior'][0])
-    assert(schedules_before['occupants'][0] != schedules_after['occupants'][0])
-    assert(schedules_before['plug_loads_other'][0] != schedules_after['plug_loads_other'][0])
-    assert(schedules_before['plug_loads_tv'][0] != schedules_after['plug_loads_tv'][0])
+    assert(schedules_before['lighting_garage'][0] == schedules_after['lighting_garage'][0])
+    assert(schedules_before['lighting_interior'][0] == schedules_after['lighting_interior'][0])
+    assert(schedules_before['occupants'][0] == schedules_after['occupants'][0])
+    assert(schedules_before['plug_loads_other'][0] == schedules_after['plug_loads_other'][0])
+    assert(schedules_before['plug_loads_tv'][0] == schedules_after['plug_loads_tv'][0])
 
     # save the model
     output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + '/test_GEBAppliancesPeakPeriodShift.osm')
